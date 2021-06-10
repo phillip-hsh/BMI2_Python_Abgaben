3483e405dfa63c86213e4784f673ca755c77a995
Phillip Weidt <weidt.phillip@mh-hannover.de>
2021-06-10 10:06:39 +0200

    Kleinere Anpassungen

--- Ueb25_ZG1-2/Ueb25_Aufg4.py
+++ Ueb25_ZG1-2/Ueb25_Aufg4.py
@@ -1,46 +1,42 @@
 class Produkt:
 
-    MWST = 0.19
-    ERM_MWST = 0.7
     WAEHRUNG = '€'
     ZEILENUMBRUCH = '\n'
     PROZENT = '%'
 
-    def __init__(self, artikel_nr, bezeichnung, preis, isErmaessigt):
+    def __init__(self, artikel_nr, bezeichnung, preis):
         self.__Artikel_Nr = artikel_nr
         self.__Bezeichnung = bezeichnung
         self.__PreisNetto = preis
-        self.__Mehrwertsteuer = Produkt.ERM_MWST if (isErmaessigt) else Produkt.MWST
 
     def __str__(self):
         rep = 'Artikelnr: ' + str(self.__Artikel_Nr) + Produkt.ZEILENUMBRUCH
         rep += 'Bezeichnung: ' + str(self.__Bezeichnung) + Produkt.ZEILENUMBRUCH
         rep += 'Preis (brutto): ' + str(self.Bruttopreis) + Produkt.WAEHRUNG + Produkt.ZEILENUMBRUCH
-        rep += 'Mehrwertsteuer: ' + str(self.__Mehrwertsteuer * 100) + Produkt.PROZENT
         return rep
 
     @property
     def Artikelnr(self):
         return self.__Artikel_Nr
+    
+    @property
+    def Nettopreis(self):
+        return self.__PreisNetto
 
     @property
     def Bezeichnung(self):
         return self.__Bezeichnung
-    
-    @property
-    def Bruttopreis(self):
-        return round(self.__PreisNetto + self.__PreisNetto * self.__Mehrwertsteuer, 2)
 
 class Buch(Produkt):
+
+    MWST = 0.7
     
-    def __init__(self, artikel_nr, bezeichnung, preis, isErmaessigt, seitenzahl):
-        super().__init__(artikel_nr, bezeichnung, preis, isErmaessigt)
+    def __init__(self, artikel_nr, bezeichnung, preis, seitenzahl):
+        super().__init__(artikel_nr, bezeichnung, preis)
         self.__Seitenzahl = seitenzahl
 
     def __str__(self):
-        rep =  'Buch:\n'
-        rep += super().__str__() + super().ZEILENUMBRUCH
-        rep += 'Seitenzahl: ' + str(self.__Seitenzahl)
+        rep = 'Buch - ' + self.Bezeichnung + ' (Art.-Nr: '  + str(self.Artikelnr) + ')\t' + str(self.Bruttopreis) + ' Euro'
         return rep
 
     @property
@@ -50,23 +46,35 @@ class Buch(Produkt):
     @property
     def Bezeichnung(self):
         return super().Bezeichnung
+
+    @property
+    def Nettopreis(self):
+        return super().Nettopreis
     
     @property
     def Bruttopreis(self):
-        return super().Bruttopreis
-
+        return round(self.Nettopreis + self.Nettopreis * Buch.MWST, 2)
+        
 
 class DVD(Produkt):
 
-    def __init__(self, artikel_nr, bezeichnung, preis, isErmaessigt, fsk):
-        super().__init__(artikel_nr, bezeichnung, preis, isErmaessigt)
+    MWST = 0.19
+
+    def __init__(self, artikel_nr, bezeichnung, preis, fsk):
+        super().__init__(artikel_nr, bezeichnung, preis)
         self.__Fsk = fsk
 
-    def __str__(self):
+    def __str__Alt(self):
         rep =  'DVD:\n'
-        rep += super().__str__() + super().ZEILENUMBRUCH
+        rep += super().__str__()
         rep += 'FSK: ' + str(self.__Fsk)
         return rep
+
+
+    def __str__(self):
+        rep = 'DVD - ' + self.Bezeichnung + ' (Art.-Nr: '  + str(self.Artikelnr) + ')\t' + str(self.Bruttopreis) + ' Euro'
+        return rep
+        
 
     @property
     def Artikelnr(self):
@@ -77,8 +85,12 @@ class DVD(Produkt):
         return super().Bezeichnung
 
     @property
+    def Nettopreis(self):
+        return super().Nettopreis
+    
+    @property
     def Bruttopreis(self):
-        return super().Bruttopreis
+        return round(self.Nettopreis + self.Nettopreis * DVD.MWST, 2)
 
 class Warenkorb:
     
@@ -92,15 +104,15 @@ class Warenkorb:
         self.__Artikel.remove(object)
 
     def __str__(self):
-        rep =  'Rechnung:\n'
 
+        rep =  ''
         sum = 0
 
         for element in self.__Artikel:
 
             sum += element.Bruttopreis
-
-            rep += str(type(element).__name__) + ' - ' + element.Bezeichnung + ' (Art.-Nr: '  + str(element.Artikelnr) + ')\t' + str(element.Bruttopreis) + ' Euro'
+            
+            rep += str(element)
             rep += '\n'
         
         rep += '-----------------------------------------------\n'
@@ -111,19 +123,21 @@ class Warenkorb:
     #------------------------------------------------------------------------------------------------------
 
 
-buch1 = Buch(3445435, 'Test1', 10.99, True, 345)
-buch2 = Buch(5686757, 'Test2', 11.99, True, 456)
+buch1 = Buch(3445435, 'Test1', 10.99, 345)
+buch2 = Buch(5686757, 'Test2', 11.99, 456)
 
-dvd1 = DVD(456787, 'Test3', 12.99, False, 16)
-dvd2 = DVD(453346, 'Test4', 13.99, False, 18)
+dvd1 = DVD(456787, 'Test3', 12.99, 16)
+dvd2 = DVD(453346, 'Test4', 13.99, 18)
 
 print(dvd1)
 
 korb1 = Warenkorb()
+
 korb1.AddArtikel(buch1)
 korb1.AddArtikel(buch2)
 korb1.AddArtikel(dvd1)
 korb1.AddArtikel(dvd2)
 
 print('\n')
+
 print(korb1)
